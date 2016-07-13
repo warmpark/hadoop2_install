52cafd718d6036373cfeac5561ae616dfd625841
byunghoon park <warmpark@t3q.com>
2016-07-13 20:48:44 +0900

    init project

---
+++ Installation/uninstall-hadoop2.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+HADOOP_VERSION=2.7.2
+HADOOP_HOME="/opt/hadoop-${HADOOP_VERSION}"
+NN_DATA_DIR=/var/data/hadoop/hdfs/nn
+SNN_DATA_DIR=/var/data/hadoop/hdfs/snn
+DN_DATA_DIR=/var/data/hadoop/hdfs/dn
+YARN_LOG_DIR=/var/log/hadoop/yarn
+HADOOP_LOG_DIR=/var/log/hadoop/hdfs
+HADOOP_MAPRED_LOG_DIR=/var/log/hadoop/mapred
+# If using jdk-8u92-linux-x64.rpm, then
+# set JAVA_HOME=""
+# JAVA_HOME= /usr/lib/jvm/java-1.7.0-openjdk-1.7.0.101-2.6.6.1.el7_2.x86_64/jre/
+JAVA_HOME=""
+echo "Stopping Hadoop 2 services..."
+pdsh -w ^dn_hosts "service hadoop-datanode stop"
+pdsh -w ^snn_host "service hadoop-secondarynamenode stop"
+pdsh -w ^nn_host "service hadoop-namenode stop"
+pdsh -w ^mr_history_host "service hadoop-historyserver stop"
+pdsh -w ^yarn_proxy_host "service hadoop-proxyserver stop"
+pdsh -w ^nm_hosts "service hadoop-nodemanager stop"
+pdsh -w ^rm_host "service hadoop-resourcemanager stop"
+
+echo "Removing Hadoop 2 services from run levels..."
+pdsh -w ^dn_hosts "chkconfig --del hadoop-datanode"
+pdsh -w ^snn_host "chkconfig --del hadoop-secondarynamenode"
+pdsh -w ^nn_host "chkconfig --del hadoop-namenode"
+pdsh -w ^mr_history_host "chkconfig --del hadoop-historyserver"
+pdsh -w ^yarn_proxy_host "chkconfig --del hadoop-proxyserver"
+pdsh -w ^nm_hosts "chkconfig --del hadoop-nodemanager"
+pdsh -w ^rm_host "chkconfig --del hadoop-resourcemanager"
+
+echo "Removing Hadoop 2 startup scripts..."
+pdsh -w ^all_hosts "rm -f /etc/init.d/hadoop-*"
+
+echo "Removing Hadoop 2 distribution tarball..."
+pdsh -w ^all_hosts "rm -f /opt/hadoop-2*.tar.gz"
+
+if [ -z "$JAVA_HOME" ]; then
+  echo "Removing JDK 1.8.0_92 distribution..."
+  pdsh -w ^all_hosts "rm -f /opt/jdk*"
+
+  echo "Removing JDK 1.8.0_92 artifacts..."
+  pdsh -w ^all_hosts "rm -f sun-java*"
+  pdsh -w ^all_hosts "rm -f jdk*"
+fi
+
+echo "Removing Hadoop 2 home directory..."
+pdsh -w ^all_hosts "rm -Rf $HADOOP_HOME"
+
+echo "Removing Hadoop 2 bash environment setting..."
+pdsh -w ^all_hosts "rm -f /etc/profile.d/hadoop.sh"
+
+echo "Removing Java bash environment setting..."
+pdsh -w ^all_hosts "rm -f /etc/profile.d/java.sh"
+
+echo "Removing /etc/hadoop link..."
+pdsh -w ^all_hosts "rm /etc/hadoop"
+
+echo "Removing Hadoop 2 command links..."
+pdsh -w ^all_hosts "rm /usr/bin/container-executor"
+pdsh -w ^all_hosts "rm /usr/bin/hadoop*"
+pdsh -w ^all_hosts "rm /usr/bin/hdfs*"
+pdsh -w ^all_hosts "rm /usr/bin/mapred*"
+pdsh -w ^all_hosts "rm /usr/bin/rcc*"
+pdsh -w ^all_hosts "rm /usr/bin/test-container-executor"
+pdsh -w ^all_hosts "rm /usr/bin/yarn*"
+
+echo "Removing Hadoop 2 script links..."
+pdsh -w ^all_hosts "rm /usr/libexec/hadoop-config.*"
+pdsh -w ^all_hosts "rm /usr/libexec/hdfs-config.*"
+pdsh -w ^all_hosts "rm /usr/libexec/httpfs-config.*"
+pdsh -w ^all_hosts "rm /usr/libexec/mapred-config.*"
+pdsh -w ^all_hosts "rm /usr/libexec/yarn-config.*"
+pdsh -w ^all_hosts "rm /usr/libexec/kms-config.*"
+
+echo "Uninstalling JDK 1.8.0_92 RPM..."
+pdsh -w ^all_hosts "rpm -ev jdk1.8.0_92"
+
+echo "Removing NameNode data directory..."
+pdsh -w ^nn_host "rm -Rf $NN_DATA_DIR"
+
+echo "Removing Secondary NameNode data directory..."
+pdsh -w ^snn_host "rm -Rf $SNN_DATA_DIR"
+
+echo "Removing DataNode data directories..."
+pdsh -w ^dn_hosts "rm -Rf $DN_DATA_DIR"
+
+echo "Removing YARN log directories..."
+pdsh -w ^all_hosts "rm -Rf $YARN_LOG_DIR"
+
+echo "Removing HDFS log directories..."
+pdsh -w ^all_hosts "rm -Rf $HADOOP_LOG_DIR"
+
+echo "Removing MapReduce log directories..."
+pdsh -w ^all_hosts "rm -Rf $HADOOP_MAPRED_LOG_DIR"
+
+echo "Removing hdfs system account..."
+pdsh -w ^all_hosts "userdel -r hdfs"
+
+echo "Removing mapred system account..."
+pdsh -w ^all_hosts "userdel -r mapred"
+
+echo "Removing yarn system account..."
+pdsh -w ^all_hosts "userdel -r yarn"
+
+echo "Removing hadoop system group..."
+pdsh -w ^all_hosts "groupdel hadoop"
+
