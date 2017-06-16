fe6ec9c4539d80d3727f90e40a1c1c46758b0685
Byunghoon Park <warmpark@t3q.com>
2017-06-17 02:16:02 +0900

    Oracle JDK Version Chanage

--- Installation/install-hadoop2-ha.sh
+++ Installation/install-hadoop2-ha.sh
@@ -165,7 +165,9 @@ if [ -z "$JAVA_HOME" ]; then
   ## JDK DOWNLOAD
     if [ ! -e "$JDK_RPM_NAME" ]; then
         echo "JDK PRM File does not exist"
-        wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "${JDK_DOWNLOAD_URI}"
+        wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" ${JDK_DOWNLOAD_URI}
+		## wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm"
+
   else 
         echo "JDK PRM File exists"
     fi
