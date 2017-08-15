f5492fd620561f7bdcc35ce6d954f94cc1f7e1de
Byunghoon Park <warmpark@t3q.com>
2017-08-15 09:04:27 +0900

    0000

--- Installation/install-hadoop2-ha.sh
+++ Installation/install-hadoop2-ha.sh
@@ -126,7 +126,6 @@ install()
 	pdcp -w ^all_hosts kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz /opt
 	pdcp -w ^all_hosts apache-storm-${STORM_VERSION}.tar.gz /opt
 	pdcp -w ^all_hosts nifi-${NIFI_VERSION}-bin.tar.gz /opt  
-	pdsh -w ^all_hosts "su - hdfs -c 'cp -f ${PHOENIX_HOME}/apache-phoenix-${PHOENIX_VERSION}-HBase-${PHOENIX_HBASE_VERSION}-server.jar  $HBASE_HOME/lib '"
 		
 	
 if [ -z "$JAVA_HOME" ]; then
@@ -181,7 +180,7 @@ fi
 	
 	echo "Extracting PHOENIX apache-phoenix-${PHOENIX_VERSION}-HBase-${PHOENIX_HBASE_VERSION}-bin.tar.gz distribution on all hosts..."
 	pdsh -w ^zk_hosts  "tar -xzf /opt/apache-phoenix-${PHOENIX_VERSION}-HBase-${PHOENIX_HBASE_VERSION}-bin.tar.gz -C /opt && chown -R hdfs:hadoop ${PHOENIX_HOME}"
-	pdsh -w ^all_hosts "su - hdfs -c 'cp -f ${PHOENIX_HOME}/phoenix-${PHOENIX_VERSION}-HBase-${PHOENIX_HBASE_VERSION}-server.jar  $HBASE_HOME/lib '"
+	pdsh -w ^all_hosts "su - hdfs -c 'cp -f ${PHOENIX_HOME}/apache-phoenix-${PHOENIX_VERSION}-HBase-${PHOENIX_HBASE_VERSION}-server.jar  $HBASE_HOME/lib '"
 
 	
 	
